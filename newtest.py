080ae5a1e18ea21a62376a65f221b24399cbe35c
riskgod <610048251@qq.com>
2017-03-14 14:24:07 +0800

    add test plan

--- newtest.py
+++ newtest.py
@@ -30,69 +30,69 @@ with open('./vender/finalComputeAllData.json') as finalComputeAll:
 
 
 def qp(l):
-    l.client.get("/qp")
+    l.client.get("qp")
 
 def getRandomAndRSAPublicKey(l):
-    l.client.get("/getRandomAndRSAPublicKey")
+    l.client.get("getRandomAndRSAPublicKey")
 
 def quotation(l):
-    l.client.post("/quotation", {"reqType":"PS", "occupationCode":"", "residencyCode":"1", "age":"1"})
+    l.client.post("quotation", {"reqType":"PS", "occupationCode":"", "residencyCode":"1", "age":"1"})
 
 def assignAgent(l):
-    l.client.post("/assignAgent", assignAgentData)
+    l.client.post("assignAgent", assignAgentData)
 
 def finalQuotation(l):
-    l.client.post("/finalQuotation", finalQuotationData)    
+    l.client.post("finalQuotation", finalQuotationData)    
 
 def createPa(l):
-    l.client.post("/createPa", createPaData)    
+    l.client.post("createPa", createPaData)    
 
 def createPs(l):
-    l.client.post("/createPs", createPsData)
+    l.client.post("createPs", createPsData)
 
 def createPfc(l):
-    l.client.post("/createPfc", createPfcData)    
+    l.client.post("createPfc", createPfcData)    
 
 def submitEsub(l):
-    l.client.post("/submitEsub", submitEsubData)  
+    l.client.post("submitEsub", submitEsubData)  
 
 def computeAll(l):
-    l.client.post("/computeAll", {"age":"34", "gender":"M", "smoker":False, "selectedTerm":15, "selectedOptionCode":3, "selectedOptionVal": 2, "totalYearlyPremium": 6000, "sumAssured": 0.0})
+    l.client.post("computeAll", {"age":"34", "gender":"M", "smoker":False, "selectedTerm":15, "selectedOptionCode":3, "selectedOptionVal": 2, "totalYearlyPremium": 6000, "sumAssured": 0.0})
 
 def fetchAppData(l):
-    l.client.post("/fetchAppData", {"erefNo": "11905CODNG01"})    
+    l.client.post("fetchAppData", {"erefNo": "11905CODNG01"})    
 
 def occupationList(l):
-    l.client.post("/occupationList", { "reqType": "PA", "occupationCode": "1"})      
+    l.client.post("occupationList", { "reqType": "PA", "occupationCode": "1"})      
 
 def occupationSpecial(l):
-    l.client.post("/occupationSpecial", { "occupationId": "APER"})  
+    l.client.post("occupationSpecial", { "occupationId": "APER"})  
 
 def dropdowns(l):
-    l.client.post("/dropdowns", { "dropDownCode": "PFCOPT066"})  
+    l.client.post("dropdowns", { "dropDownCode": "PFCOPT066"})  
 
 def financials(l):
-    l.client.post("/financials", { "dropDownCode": "OCB"})  
+    l.client.post("financials", { "dropDownCode": "OCB"})  
 
 def addmail(l):
-    l.client.post("/addmail", addmailData)  
+    l.client.post("addmail", addmailData)  
 
 def postalcode(l):
-    l.client.post("/postalcode", { "postalCode": "649185" })  
+    l.client.post("postalcode", { "postalCode": "649185" })  
 
 def makepayment(l):
-    l.client.post("/makepayment", {"amt":"6045.78", "ref":"03137CODNG01", "cur":"SGD", "returnUri":"/payment_return"})
+    l.client.post("makepayment", {"amt":"6045.78", "ref":"03137CODNG01", "cur":"SGD", "returnUri":"/payment_return"})
 
 def finalComputeAll(l):
-    l.client.post("/finalComputeAll", finalComputeAllData)    
+    l.client.post("finalComputeAll", finalComputeAllData)    
 
 @task
 def uploadImage(self):
-    response = self.client.get("/uploadImage")
+    response = self.client.get("uploadImage")
     csrftoken = response.cookies['csrftoken']
     attach = open('./vender/test.jpg', 'rb')
 
-    r = self.client.post("/uploadImage", data={
+    r = self.client.post("uploadImage", data={
         'csrfmiddlewaretoken': csrftoken,
         'password': smart_str(u'wkefjgui'),
         'payload': smart_str(u'kjsdgfljdsh'),
